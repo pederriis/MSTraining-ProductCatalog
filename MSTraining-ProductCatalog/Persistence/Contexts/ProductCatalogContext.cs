623ed896365d729fdc356d6198fad1b52ef8b2d6
pri <pri@USD1931>
2021-11-02 16:03:02 +0100

    Add project files.

---
+++ MSTraining-ProductCatalog/Persistence/Contexts/ProductCatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MSTraining_ProductCatalog.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSTraining_ProductCatalog.Persistence.Contexts
+{
+    public class ProductCatalogContext : DbContext
+    {
+        public ProductCatalogContext(DbContextOptions<ProductCatalogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductEF> ProductCatalogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductEF>().ToTable("Products");
+        }
+    }
+}
---
+++ MSTraining-ProductCatalog/Persistence/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSTraining_ProductCatalog.Persistence.Models
+{
+    public class ProductEF
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
