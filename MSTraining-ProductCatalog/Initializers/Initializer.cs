6af032840be68609b077b8a7104b18ea623a8c26
Peder Riis <pederriis@gmail.com>
2021-11-04 09:46:50 +0100

    seedder

---
+++ MSTraining-ProductCatalog/Initializers/Initializer.cs
@@ -0,0 +1,44 @@
+﻿using MSTraining_ProductCatalog.Persistence.Contexts;
+using MSTraining_ProductCatalog.Persistence.Models;
+using MSTraining_ProductCatalog.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSTraining_ProductCatalog.Initializers
+{
+    
+    public class Initializer
+    {
+      
+
+        public static void SeedDatabase(ProductCatalogContext productCatalogContext)
+        {
+
+            if (productCatalogContext.Products.Count()==0)
+            {
+
+                List<ProductEF> products = new List<ProductEF> {
+                new ProductEF { Name = "Havregryn", Price = 10 },
+                new ProductEF { Name = "Tandpasta", Price = 20 },
+                new ProductEF { Name = "Mælk", Price = 15 },
+                new ProductEF { Name = "Pasta", Price = 20 },
+                new ProductEF { Name = "Leverposteg", Price = 17 },
+                new ProductEF { Name = "Kogt torsk", Price = 50 },
+
+            };
+
+                foreach (var p in products)
+                {
+                    productCatalogContext.Add(p);
+                }
+                productCatalogContext.SaveChanges();
+            }
+
+            
+
+
+        }
+    }
+}
--- MSTraining-ProductCatalog/Program.cs
+++ MSTraining-ProductCatalog/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MSTraining_ProductCatalog.Initializers;
+using MSTraining_ProductCatalog.Persistence.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,36 @@ namespace MSTraining_ProductCatalog
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args).Build().Run();
+        //}
+
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+
+
+           // CreateDbIfNotExists(host);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+
+                try
+                {
+                    var context = services.GetRequiredService<ProductCatalogContext>();
+                    Initializer.SeedDatabase(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +51,32 @@ namespace MSTraining_ProductCatalog
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+     
+                
                 });
+
+
+
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ProductCatalogContext>();
+                    Initializer.SeedDatabase(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
     }
+
+
+
 }
